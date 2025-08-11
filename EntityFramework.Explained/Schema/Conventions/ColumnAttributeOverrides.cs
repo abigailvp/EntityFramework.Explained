c706d5b194fb451844ddf585608248ff04079a59
abigailvp <abigail.van.peteghem@gmail.com>
2025-08-11 14:32:53 +0200

    feat(schema): comments verwerkt

--- EntityFramework.Explained/Schema/Conventions/ColumnAttributeOverrides.cs
+++ EntityFramework.Explained/Schema/Conventions/ColumnAttributeOverrides.cs
@@ -11,13 +11,13 @@ using QuickPulse.Show;
 
 namespace EntityFramework.Explained.Schema.Conventions;
 
-[DocFile] //maakt van classnaam automatisch een titel in doc
+[DocFile]
 public class ColumnAttributesOverrides
 {
     public class Student
     {
         [Column("Id", Order = 0)]
-        public int StudentId { get; set; } = default!; //met of zonder KLASSEnaam (dus hier: Student)
+        public int StudentId { get; set; } = default!;
 
         [Column("Age", Order = 2)]
         public int StudentAge { get; set; }
@@ -30,11 +30,10 @@ public class ColumnAttributesOverrides
 
     }
 
-
     [Fact]
     [DocHeader("Sql Server")]
     [DocContent("Generates `nvarchar(max)`.")]
-    public void SqlServer()
+    public void SqlServerFollowsOrder_ChangesColumnName()
     {
         using var context = new TestSqlServerContext<Student>();
         var sql = context.Database.GenerateCreateScript();
@@ -43,42 +42,16 @@ public class ColumnAttributesOverrides
 
         Assert.Equal("CREATE TABLE [Items] (", reader.NextLine());
         Assert.Equal("    [Id] int NOT NULL IDENTITY,", reader.NextLine());
-        Assert.Equal("    [Name] nvarchar(max) NOT NULL,", reader.NextLine());
-        Assert.Equal("    [Age] int NOT NULL,", reader.NextLine());
+        Assert.Equal("    [Age] int NOT NULL,", reader.SkipToLineContaining("int"));
         Assert.Equal("    [Birthdate] datetime2 NOT NULL,", reader.NextLine());
         Assert.Equal("    CONSTRAINT [PK_Items] PRIMARY KEY ([Id])", reader.NextLine());
-
-
-    }
-
-    [Fact]
-    [DocHeader("Sql Server")]
-    [DocContent("Generates `nvarchar(max)`.")]
-    public void SqlServerFollowsOrder()
-    {
-        using var context = new TestSqlServerContext<Student>();
-        var sql = context.Database.GenerateCreateScript();
-        var reader = LinesReader.FromText(sql);
-        // reader.AsAssertsToLogFile();
-
-        Assert.Equal("CREATE TABLE [Items] (", reader.NextLine());
-        Assert.Equal("    [Id] int NOT NULL IDENTITY,", reader.NextLine());
-        Assert.Equal("    [Age] int NOT NULL,", reader.SkipToLineContaining("int")); //naam staat op lijn hiervoor want order=1
-        Assert.Equal("    [Birthdate] datetime2 NOT NULL,", reader.NextLine());
-        Assert.Equal("    CONSTRAINT [PK_Items] PRIMARY KEY ([Id])", reader.NextLine());
-        Assert.Equal(");", reader.NextLine());
-        Assert.Equal("GO", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.Equal("", reader.NextLine());
-        Assert.True(reader.EndOfContent());
 
     }
 
     [Fact]
     [DocHeader("Sqlite")]
     [DocContent("Generates `TEXT`.")]
-    public void Sqlite()
+    public void SqliteFollowsOrder_ChangesColumnName_AndHasType()
     {
         using var context = new TestSqliteContext<Student>();
         var sql = context.Database.GenerateCreateScript();
@@ -86,6 +59,6 @@ public class ColumnAttributesOverrides
         // reader.AsAssertsToLogFile();
         Assert.Contains("\"Name\" TEXT NOT NULL", reader.SkipToLineContaining("Name"));
         Assert.Contains("\"Age\" INTEGER NOT NULL", reader.NextLine());
-        Assert.Contains("\"Birthdate\" TEXT NOT NULL", reader.NextLine()); //geen datetime!!
+        Assert.Contains("\"Birthdate\" TEXT NOT NULL", reader.NextLine());
     }
 }
