5f97223b8e899e69063f88ed8803f926dd56eac7
abigailvp <abigail.van.peteghem@gmail.com>
2025-08-12 11:42:52 +0200

    feat(schema): 2 tests for tph #21

--- EntityFramework.Explained/Schema/Inheritance/DiscriminatorColumn.cs
+++ EntityFramework.Explained/Schema/Inheritance/DiscriminatorColumn.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using EntityFramework.Explained._Tools.Helpers;
 using EntityFramework.Explained._Tools.TestContexts;
 using Microsoft.EntityFrameworkCore;
@@ -8,9 +7,9 @@ using QuickPulse.Explains.Text;
 namespace EntityFramework.Explained.Schema.Conventions;
 
 [DocFile]
-public class TablePerHierarchy
+public class DiscriminatorColumn
 {
-    public class AnimalSqliteDbContext<T> : DbContext where T : class
+    public class AnimalDbContext<T> : DbContext where T : class
     {
         public DbSet<Animal> Animals => Set<Animal>();
 
@@ -21,23 +20,10 @@ public class TablePerHierarchy
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Animal>()
-            .Property<int?>("numberOfMeows");
+            .HasDiscriminator<string>("AnimalType")
+            .HasValue<Cat>("Cat")
+            .HasValue<Dog>("Dog");
 
-            modelBuilder.Entity<Animal>()
-            .Property<int?>("numberOfBarks");
-        }
-    }
-
-    public class AnimalServerDbContext<T> : DbContext where T : class
-    {
-        public DbSet<Animal> Animals => Set<Animal>();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlServer("DoesNotMatter");
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Animal>()
             .Property<int?>("numberOfMeows");
 
@@ -47,9 +33,7 @@ public class TablePerHierarchy
     }
     public class Animal
     {
-        [Required]
         public int Id { get; set; }
-
         public string isPet { get; set; }
     }
 
@@ -65,33 +49,33 @@ public class TablePerHierarchy
 
     [Fact]
     [DocHeader("Sql Server")]
-    [DocContent("has tph with inherited properties of base class and properties of derived classes.")]
-
-    public void SqlServer_Has_TablePerHierarchy()
+    [DocContent("has tph with AnimalType as discriminator and inherited properties of base class and properties of derived classes.")]
+    [DocSnippet("Property with type bool will generate an int in the database in SQL Server (output 0 or 1). So we used type string instead.")]
+    public void SqlServer()
     {
-        using var context = new AnimalServerDbContext<Animal>();
-        var sql = context.Database.GenerateCreateScript();
-        var reader = LinesReader.FromText(sql);
-
-        Assert.Equal("    [Id] int NOT NULL IDENTITY,", reader.SkipToLineContaining("Id"));
-        Assert.Equal("    [isPet] nvarchar(max) NOT NULL,", reader.NextLine());
-        Assert.Equal("    [numberOfBarks] int NULL,", reader.NextLine());
-        Assert.Equal("    [numberOfMeows] int NULL,", reader.NextLine());
-
-    }
-
-    [Fact]
-    [DocHeader("Sqlite")]
-    [DocContent("has tph with inherited properties of base class and properties of derived classes.")]
-    public void Sqlite_Has_TablePerHierarchy()
-    {
-        using var context = new AnimalSqliteDbContext<Animal>();
-
+        using var context = new AnimalDbContext<Animal>();
         var sql = context.Database.GenerateCreateScript();
         var reader = LinesReader.FromText(sql);
 
         Assert.Equal("    \"Id\" INTEGER NOT NULL CONSTRAINT \"PK_Animals\" PRIMARY KEY AUTOINCREMENT,", reader.SkipToLineContaining("Id"));
         Assert.Equal("    \"isPet\" TEXT NOT NULL,", reader.NextLine());
+        Assert.Equal("    \"AnimalType\" TEXT NOT NULL,", reader.NextLine());
+        Assert.Equal("    \"numberOfBarks\" INTEGER NULL,", reader.NextLine());
+        Assert.Equal("    \"numberOfMeows\" INTEGER NULL", reader.NextLine());
+    }
+
+    [Fact]
+    [DocHeader("Sqlite")]
+    [DocContent("has tph with AnimalType as discriminator and inherited properties of base class and properties of derived classes")]
+    public void Sqlite()
+    {
+        using var context = new AnimalDbContext<Animal>();
+        var sql = context.Database.GenerateCreateScript();
+        var reader = LinesReader.FromText(sql);
+
+        Assert.Equal("    \"Id\" INTEGER NOT NULL CONSTRAINT \"PK_Animals\" PRIMARY KEY AUTOINCREMENT,", reader.SkipToLineContaining("Id"));
+        Assert.Equal("    \"isPet\" TEXT NOT NULL,", reader.NextLine());
+        Assert.Equal("    \"AnimalType\" TEXT NOT NULL,", reader.NextLine());
         Assert.Equal("    \"numberOfBarks\" INTEGER NULL,", reader.NextLine());
         Assert.Equal("    \"numberOfMeows\" INTEGER NULL", reader.NextLine());
     }
