e1df7c05ad0cecdfa05386447bac2f7890f0cf64
abigailvp <abigail.van.peteghem@gmail.com>
2025-08-12 11:53:52 +0200

    feat(schema): remove unnecessary usings #21

--- EntityFramework.Explained/Schema/Inheritance/TablePerHierarchy.cs
+++ EntityFramework.Explained/Schema/Inheritance/TablePerHierarchy.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using EntityFramework.Explained._Tools.Helpers;
-using EntityFramework.Explained._Tools.TestContexts;
 using Microsoft.EntityFrameworkCore;
 using QuickPulse.Explains;
 using QuickPulse.Explains.Text;
